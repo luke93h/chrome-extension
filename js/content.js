42bf939df77606a73b1c294a2144abed4e0ab73d
luke <543564013@qq.com>
2018-10-10 19:02:04 +0800

    first commit

---
+++ js/content.js
@@ -0,0 +1,55 @@
+(function(){
+  var listeners = []
+  var iframe = document.getElementById('contentIFrame0')
+  var intervalCode = 0
+  iframe.addEventListener('load', function(){
+    for(var i= 0;i<listeners.length;i++){
+      listeners.shift()()
+    }
+  })
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    // console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
+    if(request.type == 'start') {
+      var _document = iframe.contentWindow.document;
+      var codeList = request.payload.code.split(',')
+      var numberList = request.payload.number.split(',')
+      var interval = parseInt(request.payload.interval) || 6
+      var i = 0
+      clearInterval(intervalCode)
+      var cb = function(){
+        var _document = document.getElementById('contentIFrame0').contentWindow.document;
+        var code = codeList[i]
+        var number = 0
+        if(i>=numberList.length){
+          number = numberList[numberList.length-1]
+        }else{
+          number = numberList[i]
+        }
+        listeners.push(function(){
+          var list = _document.querySelectorAll('#tableList tr.odd-row td')
+          var name = list[1].innerText
+          var stock = list[list.length-1].innerText
+          var info = {name: name, stock: stock, code: code, number: number, time: getNowTime() }
+          sendMessageToPopup({type:'refresh', payload:info});
+        })
+        _document.getElementById('txtProduct').value=codeList[i]
+        _document.getElementById('btnSearch').click()
+        if(i>=codeList.length-1){
+          i=0
+        }else{
+          i++
+        }
+      }
+      cb()
+      intervalCode = setInterval(cb, interval*1000)
+    }
+  });
+  
+  function sendMessageToPopup(message){
+    chrome.runtime.sendMessage('gcjefpdikjlgpndiafaiffmbnjdphkph', message)
+  }
+  function getNowTime(){
+    var d = new Date()
+    return d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
+  }
+})()
---
+++ js/popup.js
@@ -0,0 +1,178 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback called when the URL of the current tab
+ *   is found.
+ */
+
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, (tabs) => {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+  // Most methods of the Chrome extension APIs are asynchronous. This means that
+  // you CANNOT do something like this:
+  //
+  // var url;
+  // chrome.tabs.query(queryInfo, (tabs) => {
+  //   url = tabs[0].url;
+  // });
+  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+/**
+ * Change the background color of the current page.
+ *
+ * @param {string} color The new background color.
+ */
+
+function startPoll(infos) {
+  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
+  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
+  // into a page. Since we omit the optional first argument "tabId", the script
+  // is inserted into the active tab of the current window, which serves as the
+  // default.
+  
+  sendMessageToContentScript({type:'start', payload:infos}, function(response){
+  });
+}
+
+/**
+ * Gets the saved background color for url.
+ *
+ * @param {string} url URL whose background color is to be retrieved.
+ * @param {function(string)} callback called with the saved background color for
+ *     the given url on success, or a falsy value if no color is retrieved.
+ */
+function getSaved(url, callback) {
+  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
+  // for chrome.runtime.lastError to ensure correctness even when the API call
+  // fails.
+  chrome.storage.sync.get(url, (items) => {
+    callback(chrome.runtime.lastError ? null : items[url]);
+  });
+}
+
+/**
+ * Sets the given background color for url.
+ *
+ * @param {string} url URL for which background color is to be saved.
+ * @param {string} color The background color to be saved.
+ */
+function saveInputValue(url, infos) {
+  var items = {};
+  items[url] = infos;
+  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
+  // optional callback since we don't need to perform any action once the
+  // background color is saved.
+  chrome.storage.sync.set(items);
+}
+
+// This extension loads the saved background color for the current tab if one
+// exists. The user can select a new background color from the dropdown for the
+// current page, and it will be saved as part of the extension's isolated
+// storage. The chrome.storage API is used for this purpose. This is different
+// from the window.localStorage API, which is synchronous and stores data bound
+// to a document's origin. Also, using chrome.storage.sync instead of
+// chrome.storage.local allows the extension data to be synced across multiple
+// user devices.
+document.addEventListener('DOMContentLoaded', () => {
+  getCurrentTabUrl((url) => {
+    var startBtn = document.getElementById('start');
+    var codeInput = document.getElementById('code');
+    var numberInput = document.getElementById('number');
+    var intervalInput = document.getElementById('number');
+
+    // Load the saved background color for this page and modify the dropdown
+    // value, if needed.
+    getSaved(url, (infos) => {
+      if (infos) {
+        startPoll(infos);
+        codeInput.value = infos.code;
+        numberInput.value = infos.number;
+        intervalInput.value = infos.intervalInput;
+      }
+    });
+
+    // Ensure the background color is changed and saved when the dropdown
+    // selection changes.
+    startBtn.addEventListener('click', () => {
+      var infos = {
+        code: codeInput.value,
+        number: numberInput.value,
+        intervalInput: intervalInput.value
+      }
+      startPoll(infos);
+      saveInputValue(url, {
+        code: codeInput.value,
+        number: numberInput.value,
+        intervalInput: intervalInput.value
+      });
+    });
+  });
+});
+
+function sendMessageToContentScript(message, callback){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		chrome.tabs.sendMessage(tabs[0].id, message, function(response){
+			if(callback) callback(response);
+		});
+	});
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+  // console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
+  if(request.type == 'refresh') {
+    var payload = request.payload
+    var trDom = document.querySelectorAll('#' + payload.code)
+    if(trDom.length > 0){
+      console.log(payload, trDom[0].querySelectorAll('td.time')[0])
+      trDom[0].querySelectorAll('td.stock')[0].innerHTML = payload.stock
+      trDom[0].querySelectorAll('td.time')[0].innerHTML = payload.time
+    }else{
+      document.querySelectorAll('#result table tbody')[0].appendChild(createTr(payload))
+    }
+  }
+});
+function createTr(info){
+  var tr = document.createElement("tr")
+  tr.id = info.code
+  tr.innerHTML = 
+    '<td class="name">' +
+      info.name +
+    '</td>' +
+    '<td class="stock">' +
+      info.stock +
+    '</td>' +
+    '<td class="time">' +
+      info.time +
+    '</td>' 
+    return tr
+}
